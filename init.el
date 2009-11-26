5d80a0645444b2ba84ded5c3187bdc7df5179227
Markus Nilsson <markus@mnilsson.se>
2009-11-26 13:12:32 +0100

    Removed some old unused code.

--- init.el
+++ init.el
@@ -8,17 +8,10 @@
 (tool-bar-mode -1) 
 
 
-
-;; (set-face-background 'region "yellow");
-;; (set-background-color "wheat3");
-
-;;(load "php-mode.el")
 (autoload 'php-mode "php-mode" "Major mode for editing PHP code." t)
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-;;(add-to-list 'auto-mode-alist '("\\.tpl\\'" . sgml-mode))
-;;(set php-mode-force-pear 1)
 (global-font-lock-mode 3)
-;; (add-hook 'php-mode-hook 'turn-on-font-lock)
+
 (global-set-key "\M- " 'dabbrev-expand)
 (ido-mode t)
 (cua-mode t)
