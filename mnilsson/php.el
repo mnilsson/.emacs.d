ce23324c807623379af4b1d668620a2b854100e9
Markus Nilsson <markus@mnilsson.se>
2010-08-08 23:07:23 +0200

    Changed back to php-mode

--- mnilsson/php.el
+++ mnilsson/php.el
@@ -1,11 +1,20 @@
 
-(load "vendor/php-mode-new")
+(vendor 'php-mode)
 
 (autoload 'php-mode "php-mode" "Major mode for editing PHP code." t)
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . php-mode))
 (autoload 'javascript-mode "javascript" "Major mode for editing javascript code." t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
 
+(setq php-basic-offset 4)
+
 
 (setq php-mode-force-pear t)
+
+
+(add-hook 'php-mode-hook
+          (lambda ()
+            (define-key php-mode-map "\C-m" 'reindent-then-newline-and-indent)
+            (define-key php-mode-map "[" 'electric-pair)
+            (define-key php-mode-map "{" 'electric-pair)))
