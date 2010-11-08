a3590b538f0e3f883c58f17b7a8abe0400ca5a0d
Markus Nilsson <markus@mnilsson.se>
2010-11-08 15:43:21 +0100

    Wrong path for theme

--- mnilsson/theme.el
+++ mnilsson/theme.el
@@ -1,5 +1,5 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 
-(load-file "../vendor/themes/color-theme-railscasts/color-theme-railscasts.el")
+(load-file "~/.emacs.d/vendor/themes/color-theme-railscasts/color-theme-railscasts.el")
 (color-theme-railscasts)
