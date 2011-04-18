1416a2e1801618d252946106acd8dbda1421177d
Markus Nilsson <markus@mnilsson.se>
2011-04-18 16:55:20 +0200

    Org-mode!!!

--- mnilsson/org-mode.el
+++ mnilsson/org-mode.el
@@ -1,3 +1,6 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/org-mode")
+(require 'org-install)
+(require 'org-habit)
 (transient-mark-mode 1)
 (setq org-log-done t)
 (add-hook 'org-mode-hook 'turn-on-font-lock)
@@ -9,7 +12,6 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-use-fast-todo-selection t)
 
-
 (setq org-treat-S-cursor-todo-selection-as-state-change nil)
 
 
@@ -19,6 +21,9 @@
 (setq org-todo-keywords
            '((sequence "TODO(t)" "|" "DONE(d!/!)" "|" "STARTED(s!)")
              (sequence "WAITING(w)" "CANCELED(c!)" "DELEGATED(l!)")))
+
+(setq org-columns-default-format "%80ITEM(Task) %10Effort(Effort){:} %10Quote")
+(setq org-global-properties (quote (("Effort_ALL" . "0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00"))))
 
 (setq org-todo-keyword-faces (
                               quote (
@@ -31,3 +36,11 @@
                                      )
                                     )
       )
+
+(setq org-directory "~/org/")
+(setq org-default-notes-file (concat org-directory "refile.org"))
+(global-set-key (kbd "C-M-r") 'org-capture)
+  
+(setq org-capture-templates (quote (("t" "todo" entry (file "~/org/refile.org") "* TODO %?\n%T")
+                                      ("n" "note" entry (file "~/org/refile.org") "* %?\n%U")
+                                      ("j" "Journal" entry (file+datetree "~/org/diary.org") "* %?\n%T"))))
